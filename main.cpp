01958f878fae87d835b8fd8af139beb1b64d0dbd
andrediasn <andredias@ice.ufjf.br>
2021-08-12 14:53:09 -0300

    fix main

--- main.cpp
+++ main.cpp
@@ -13,42 +13,72 @@ int main(){
     //file = fopen("entrada.txt", "r");
 
     ifstream arq;
-    arq.open("entrada.txt");
+    arq.open("Entradas/entrada.txt");
 
     if(arq.fail()) {
         cout << "Erro na leitura do arquivo." << endl;
         exit (1);
     }
 
-    int memInst[128][32];
+    int memInst[128][32];  // Matriz da Memoria de Instrucoes
     for(int i = 0; i < 128; i++) 
-        memInst[i][0] = -1;
+        memInst[i][0] = -1; // Inicializando primeira coluna de cada linha com -1 para indicar que esta sem instrucao.
 
-    int k = 0;
-    string auxStr;
-    while(arq.good() && k < 128){
+
+    // ============= Etapa 1: IF - Instruction Fetch ============= // 
+
+    // Lendo instrucoes de entrada e salvando na Memoria de Instrucoes
+    int k = 0; // Representa a linha da matriz, PC = k * 4
+    string auxStr; // string aux para receber linha da entrada
+    while(arq.good() && k < 128){ // Enquanto nao chegou o fim do arquivo de entrada e nao foi atingido o limite de 128 instrucoes.
         getline(arq, auxStr, '\n');
-        for (int j = 0; j < 32; j++){
-            if(auxStr[j] == '0')
+        for (int j = 0; j < 32; j++){  // Matriz recebe bit a bit
+            if(auxStr[j] != '0' && auxStr[j] != '1'){ // Se ha caractere nao binario
+                int opc;
+                do{ // Loop para resolver o erro.
+                    cout << "Erro: a linha " << (j+1) << " possui caracterio nao binario." << endl;
+                    cout << " - Digite 1 para encerrar." << endl;
+                    cout << " - Digite 2 para prosseguir e ler a proxima linha de instrucoes." << endl;
+                    cout << "Indique o que deseja fazer: ";
+                    cin >> opc;
+                    if (opc == 1)
+                        exit (1); // Encerra programa
+                    else if( opc != 2)
+                        cout << " Erro: Opcao invalida!!" << endl;
+                }while(opc != 2); 
+                break; // Interrome for. Prossegue para proxima linha do arqivo de entrada
+            }
+            else if(auxStr[j] == '0')
                 memInst[k][j] = 0;
-            if(auxStr[j] == '1')
+            else
                 memInst[k][j] = 1;
         }
         k++;
     }
+    for (int i = 0; i < 32; i++) {
+        cout << memInst[0][i] << endl;
+    }
+     
         
-
     if(k == 128){
-        cout << "Atingiu o limite da memoria de instrucoes." << endl;
-        cout << "Deseja continuar a execucao com as 128 primeiras instrucoes? (y/n) ";
+        int opc;
+        do{
+            cout << "Erro: Atingiu o limite de 128 da Memoria de Instrucoes." << endl;
+            cout << " - Digite 1 para encerrar." << endl;
+            cout << " - Digite 2 para prosseguir execucao com as 128 primeiras instrucoes.";
+            if (opc == 1)
+                        exit (1); // Encerra programa
+            else if(opc != 2)
+                        cout << " Erro: Opcao invalida!!" << endl;
+        }while(opc != 2); //Prossegue para proxima linha do arqivo de entrada
     }
 
     k = 0;
     while(memInst[k][0] != -1){
 
-
+        int PC = k * 4;
         // Start Pipeline - Etapa 1 do Pipeline: IF_ID
-        IF_ID *ifid = new IF_ID(memInst[k], k*4);
+        IF_ID *ifid = new IF_ID(memInst[k], PC);
 
         // Etapa 2 do Pipiline: ID_EX
 
@@ -57,9 +87,9 @@ int main(){
         // Etapa 4 do Pipiline:
 
         // Etapa 5 do Pipiline:
+        // Aqui o valor do pc sera retornado.
+        k = PC / 4;
          
-        k++;
+        
     }
-    
-
 }
